c0fcf886a179e96ae67acbd194e8519c02a2c625
Matthew Marchand <marchandmr@icloud.com>
2020-11-14 09:49:27 -0600

    commented the javascript

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var time = moment().format('HH');
+var time = moment().format('HH'); // variables
 var hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var timeCount = 0;
 var timeIDcounter = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a')); // sets date and time on page
 
-for (timeCount; timeCount < hoursArray.length; timeCount++) {
+for (timeCount; timeCount < hoursArray.length; timeCount++) { // for loop that sets past, present, and future classes to text boxes
     var timeSlot = $("#timeSlot-" + timeIDcounter[timeCount]);
     if (hoursArray[timeCount] == time) {
         timeSlot.addClass("present");
@@ -24,6 +24,8 @@ for (timeCount; timeCount < hoursArray.length; timeCount++) {
 $(".save").on("click", saveData)
 
 function saveData() {
+    // saves the value of the text box into a variable called reminder  and saves that into it's parent which in this case is going to be  #calendar 9 -17
+    // the getItem runs in line 21 to save and load the text value of each text box
 
     var reminder = $(this).siblings("textarea").val();
     var parent = $(this).parent().attr("id");
